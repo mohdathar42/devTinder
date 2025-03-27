76b1a0906b021abd61eaf8d469bfc1e389ad3ba2
mohdathar42 <103302377+mohdathar42@users.noreply.github.com>
2025-03-27 15:15:14 +0530

    dynamic-signup

--- src/app.js
+++ src/app.js
@@ -3,23 +3,18 @@ const app = express();
 const connectDB = require("./config/database.js");
 const User = require("./model/user.js");
 
+app.use(express.json()); // Middleware to parse JSON request body
 app.post("/signup", async (req, res) => {
   //creating a new instanse of user model
-  const user = new User({
-    firstName: "mohd",
-    lastName: "sikander",
-    emailId: "mohdsikandar@gmail.com",
-    password: "1936453cls5447",
-  });
 
- try {
+  const user = new User(req.body);
+
+  try {
     //saving the user to the database
     await user.save();
-  res.send("User created successfully");
-  
-  }
-  catch (err) {
-    res.status(400).send("error saving the user "+ err.message);
+    res.send("User created successfully");
+  } catch (err) {
+    res.status(400).send("error saving the user " + err.message);
   }
 });
 
